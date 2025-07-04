fc219c3930a9b6721ae7b26c464f95bc2fae21c4
David <rynstheoverlord@gmail.com>
2025-07-04 19:12:43 +0100

    barebones

---
+++ user/js/signup.js
@@ -0,0 +1,9 @@
+
+const appwrite_url = "https://fra.cloud.appwrite.io/v1"
+const project_id = "686815220031b86fa074"
+
+const client: Appwrite.Client = new Appwrite.Client()
+										.setEndpoint(appwrite_url)
+										.setProject(project_id)
+
+
