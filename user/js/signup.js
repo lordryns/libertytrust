0f2efa58b2f0b97f2f7478c0c57fdfa75ad3697a
David <rynstheoverlord@gmail.com>
2025-07-05 04:08:25 +0100

    handling auth

--- user/js/signup.js
+++ user/js/signup.js
@@ -1,51 +1,60 @@
-
 const appwrite_url = "https://fra.cloud.appwrite.io/v1"
 const project_id = "686815220031b86fa074"
 
-const client: Appwrite.Client = new Appwrite.Client()
-										.setEndpoint(appwrite_url)
-										.setProject(project_id)
+const client = new Appwrite.Client()
+                                    .setEndpoint(appwrite_url)
+                                    .setProject(project_id)
 
 
 const account = new Appwrite.Account(client)
+
+const notyf = new Notyf();
 
 const alertDiv = document.getElementById("alert-div");
 
 document.getElementById("signup-form").addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	let firstname = document.getElementById("firstname");
-	let lastname = document.getElementById("lastname");
-	let username = document.getElementById("username");
-	let phoneno = document.getElementById("phoneno");
-	let email = document.getElementById("email");
-	let password = document.getElementById("password");
-	let confirmPassword = document.getElementById("confirm-password");
+
+	let firstname = document.getElementById("firstname").value;
+	let lastname = document.getElementById("lastname").value;
+	let username = document.getElementById("username").value;
+	let phoneno = document.getElementById("phoneno").value;
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+	let confirmPassword = document.getElementById("confirm-password").value;
 	let checkbox = document.getElementById("checkbox");
 
-	if (checkbox.checked) {
-		alertDiv.innerHTML = customAlert(message, "success")
-	}
+
+//  notyf.success(`${firstname} - ${lastname} -  ${username} -  ${email} - ${phoneno} -  ${password}`)
+	
+  var submitBtn = document.querySelector(".submit");
+
+  submitBtn.innerHTML = "Processing...";
+	if (password === confirmPassword) {
+    const promise = account.create(username, email, password, firstname + " " + lastname) 
+    promise.then(
+      res => {
+        notyf.success("Account created successfully!");
+        submitBtn.innerHTML = "Register";
+
+       try {
+          const result = await account.createEmailPasswordSession(email, password);
+          location.replace("/dashboard/index.html") 
+        } catch (error) {
+          notyf.error("Failed to log in, please try to do this manually!")
+        } 
+      }
+    ).catch(err => {
+        console.log(err);
+        notyf.error("An error occured! We were unable to create an account for you, please try again. " + err);
+        submitBtn.innerHTML = "Register";
+      })
+  } else {
+    notyf.error("Passwords don't match!");
+  }
+	
 
 })
 
 
-function customAlert(message, state) {
-	return `<div id="alert" style="
-  position:fixed;
-  top:20px;
-  right:20px;
-  background:#4caf50;
-  color:#fff;
-  padding:10px 20px;
-  border-radius:5px;
-  font-family:sans-serif;
-  box-shadow:0 2px 6px rgba(0,0,0,0.2);
-">${message}</div>
-
-<script>
-   const el = document.getElementById('alert');
-   el.style.display = 'block';
-   setTimeout(() => el.style.display = 'none', 3000)
-</script>`
-}
